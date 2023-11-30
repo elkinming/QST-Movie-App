cf41fd1de88edf3c2e1f7719d9176328e9e52094
Elkin Rativa <elkinming@gmail.com>
2023-11-30 11:30:50 -0500

    feat: Setting project structure + Navbar structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'movies',
+    component: MoviesListComponent
+  },
+  {
+    path: 'movie',
+    component: MovieDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MovieDetailsComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
